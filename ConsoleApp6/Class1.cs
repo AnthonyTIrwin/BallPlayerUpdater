c1453ebad3d0abb362e2b30e42610719c37c0a12
AnthonyTIrwin <41561146+AnthonyTIrwin@users.noreply.github.com>
2019-03-17 11:30:22 -0400

    First Commit

---
+++ ConsoleApp6/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace BallPlayerUpdater
+{
+    class DataRecord
+    {
+
+        public string Name { get; set; }
+        public string Popcorn { get; set; }
+        public string Position { get; set; }
+        public string Height { get; set; }
+        public string Weight { get; set; }
+        public string Age { get; set; }
+    }
+}
--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -1,15 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using CsvHelper;
 
-namespace ConsoleApp6
+namespace BallPlayerUpdater
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            using (var sr = new StreamReader(@"c:\temp\mlb_players.csv"))
+            {
+                var reader = new CsvReader(sr);
+
+                //CSVReader will now read the whole file into an enumerable
+                IEnumerable<DataRecord> records = reader.GetRecords<DataRecord>();
+
+                foreach (DataRecord record in records.ToList())
+                {
+                    Console.WriteLine("{0}, {1}, {2}, {3}, {4} {5}", record.Name, record.Popcorn, record.Position,
+                        record.Height, record.Weight, record.Age);
+                }
+
+
+                for (int i = 0; i < 35; i++)
+                {
+                    Console.WriteLine("potato {0}", i);
+
+                }
+                Console.ReadLine();
+            }
         }
     }
 }
+
